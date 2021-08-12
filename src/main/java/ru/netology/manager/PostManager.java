4c1dcf71756c923b628f8472c967b4dc1d7b548c
xeniya <xeniia.izotowa@gmail.com>
2021-08-12 16:52:42 +0300

    PostManager added

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,17 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId) {
+        //add logic
+        return  null;
+    }
+
+    public void delete(int ownerId, int postId) {
+        //add logic
+    }
+
+}
